2ac8758131cfc710680810bac7e592fde306a62e
stwertnikcoding <chransondub@gmail.com>
2021-07-19 14:46:24 -0600

    Finalized header styles and changed title

--- src/components/links/link-container.js
+++ src/components/links/link-container.js
@@ -7,7 +7,9 @@ export default class LinkContainer extends Component {
     return (
       <div className="link-container">
         <div className="heading-wrapper"> 
-            <h1>His Chransen's Music :</h1>
+            <div className="main-heading">
+                <h1>His Chransen's Music :</h1>
+            </div>
             <h3>Click a link to visit me there</h3>
         </div>
         <BtnLinks />
