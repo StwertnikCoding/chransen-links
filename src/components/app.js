7001224b88eec9eea75f17867066c471da8b80dd
stwertnikcoding <chransondub@gmail.com>
2021-08-02 13:08:01 -0600

    Added Cover Image structure, added download icon to download button, renamed download button and changed some styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 
 
 import Homepage from './pages/home';
 import NewRelease from './pages/new-release';
 
-library.add(fab);
+library.add(fab, faDownload);
 
 
 
--- src/components/containers/link-container.js
+++ src/components/containers/link-container.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class LinkContainer extends Component {
   render() {
     return (
       <div>
         <div className="new-release-btn-links-wrapper">
+
+        <div className="cover-image-wrapper">
+            <img className="cover-image" src="../" alt="cover"></img>
+        </div>
+
             <div className='new-release-btn-link-wrapper'>
                 <a href='https://hypeddit.com/track/t8bqbd'>
                     <button className="new-release-btn-link">
@@ -38,8 +44,12 @@ export default class LinkContainer extends Component {
             </div>
 
             <div className='new-release-btn-link-wrapper'>
+                
                 <a href='https://hypeddit.com/track/t8bqbd'>
-                    <button className="new-release-btn-link">
+                    <button className="download-new-release-btn-link">
+                        <div className="download-icon">
+                            <FontAwesomeIcon icon="download" />
+                        </div>
                         His Chransen - The Order [800 FOLLOWER FREE DL]
                     </button>
                 </a>
