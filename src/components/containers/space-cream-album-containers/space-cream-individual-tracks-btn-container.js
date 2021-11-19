7fc26efd5e8c50b9b0e423cf018d668ee04bfc61
stwertnikcoding <chransondub@gmail.com>
2021-11-19 10:08:14 -0700

    Created individual tracks page container component

---
+++ src/components/containers/space-cream-album-containers/space-cream-individual-tracks-btn-container.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+import SpaceCreamAlbumCover from '../../images/space-cream-album-cover';
+
+export default class SpaceCreamIndividualTracksButtonContainer extends Component {
+  render() {
+    return (
+        <div className="new-release-btn-links-wrapper">
+        
+        <div className="cover-image-wrapper">
+            <SpaceCreamAlbumCover />
+        </div>
+
+
+        <div className="new-release-heading">
+            <h1>Out 12/1</h1>
+        </div>
+
+        <div className='new-release-btn-link-wrapper'>
+            
+        <Link className="bigger-link" to={{ pathname: "https://soundcloud.com/chransen/ghostwobblevip-3/s-qNPZoliIKWV?si=db0e409258c44148b84850c3ec2c3adb"}} target="_blank">
+                <button className="new-release-btn-link">
+                    <div className="new-release-social-icon"> 
+                        <FontAwesomeIcon icon={['fab', 'soundcloud']} />
+                    </div>
+
+                    His Chransen - Space Cream EP on Soundcloud
+                </button>
+            </Link>
+        </div>
+
+        <div className='new-release-btn-link-wrapper'>
+            
+            <Link className="bigger-link" to={{ pathname: "https://www.youtube.com/watch?v=dHAVPoXWSn0" }} target="_blank">
+                <button className="new-release-btn-link">
+                    <div className="new-release-social-icon"> 
+                        <FontAwesomeIcon icon={['fab', 'youtube']} />
+                    </div>
+
+                    His Chransen - Space Cream EP (Youtube Trailer)
+
+                </button>
+            </Link>
+        </div>
+        
+
+        <div className='new-release-btn-link-wrapper'>
+            
+            <Link className="bigger-link" to="/nuh-uh" target="_blank">
+                <button className="new-release-btn-link">
+                    <div className="new-release-social-icon"> 
+                        <FontAwesomeIcon icon={['fab', 'spotify']} />
+                    </div>
+
+                    His Chransen - Space Cream EP on Spotify
+
+                </button>
+            </Link>
+        </div>
+
+        <div className='new-release-btn-link-wrapper'>
+            
+        <Link className="bigger-link" to="/nuh-uh" target="_blank">
+                <button className="new-release-btn-link">
+                    <div className="new-release-social-icon"> 
+                        <FontAwesomeIcon icon={['fab', 'apple']} />
+                    </div>
+
+                    His Chransen - Space Cream EP on Apple Music
+
+                </button>
+            </Link>
+        </div>
+
+        <div className='new-release-btn-link-wrapper'>
+            
+            <Link className="bigger-link" to="/nuh-uh" target="_blank">
+                <button className="new-release-btn-link">
+                    <div className="new-release-social-icon"> 
+                        <FontAwesomeIcon icon={['fab', 'amazon']} />
+                    </div>
+
+                    His Chransen - Space Cream EP on Amazon
+
+                </button>
+            </Link>
+        </div>
+
+        <div className='new-release-btn-link-wrapper'>
+            
+            <Link className="bigger-link" to={{ pathname: "https://hypeddit.com/o7uxa5"}} target="_blank">
+                <button className="new-release-btn-link">
+                    <div className="new-release-social-icon"> 
+                        <FontAwesomeIcon icon='download' />
+                    </div>
+                    His Chransen - Space Cream EP [FREE DL]
+                </button>
+            </Link>
+        </div>
+       
+        <div className='homepage-link-wrapper'>
+            <Link className="go-back-link" to="/new-releases">
+                Go Back
+            </Link>
+        </div>
+
+        <div className='homepage-link-wrapper'>
+            <Link className="homepage-link" to="/">
+                Return to Homepage
+            </Link>
+        </div>
+</div>
+    );
+  }
+}
--- src/components/pages/recent-releases/space-cream-album/space-cream-individual-tracks.js
+++ src/components/pages/recent-releases/space-cream-album/space-cream-individual-tracks.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import HeadingContainer from '../../../containers/heading-container';
-import SpaceCreamFullAlbumButtonContainer from '../../../containers/space-cream-album-containers/space-cream-full-album-button-container';
+import SpaceCreamIndividualTracksButtonContainer from '../../../containers/space-cream-album-containers/space-cream-individual-tracks-btn-container';
 import IconContainer from '../../../containers/icon-container';
 
 export default class SpaceCreamIndividualTracks extends Component {
@@ -13,7 +13,7 @@ export default class SpaceCreamIndividualTracks extends Component {
         </div>
 
         <div className="link-container-wrapper">
-            <SpaceCreamFullAlbumButtonContainer />
+            <SpaceCreamIndividualTracksButtonContainer />
         </div>
 
         <div className="icon-container-wrapper">
